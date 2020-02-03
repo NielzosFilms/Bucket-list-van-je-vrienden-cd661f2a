bbcbfe86f0636e367f4dbc253e7c624cddec593d
NielzosFilms <nielzosfilms@gmail.com>
2020-02-03 11:43:22 +0100

    Uitwerking van de exercise

--- bucket_list_friends.php
+++ bucket_list_friends.php
@@ -6,18 +6,15 @@
         exit($count . " is geen nummer!");
     }
     $dromen = array();
-    $namen = array();
     for($i=0;$i<$count;$i++){
         echo("Wat is jouw naam?".PHP_EOL);
         $naam = readline();
         echo("Wat is jouw droom?".PHP_EOL);
         $droom = readline();
-        array_push($namen, $naam);
         $dromen[$naam] = $droom;
     }
-    for($i=0;$i<count($dromen);$i++){
-        $naam = $namen[$i];
-        echo($naam." Heeft als droom: ".$dromen[$naam].PHP_EOL);
+    foreach($dromen as $key => $val){
+        echo($key. " Heeft als droom: ".$val);
     }
 
 ?>
